2caf657843ada9c0874074da0a3993f1d259eb95
AngusDobson <57667421+AngusDobson@users.noreply.github.com>
2023-05-19 14:58:25 +1000

    Register user remove pfp

--- application.py
+++ application.py
@@ -12,27 +12,9 @@ app.secret_key = os.urandom(24)
 # Configure boto3 client
 boto3.setup_default_session(region_name='ap-southeast-2')
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('users')
-
 s3 = boto3.client('s3')
+users_table = dynamodb.Table('users')
 bucket_name = 'dnd-app-bucket'
-
-def upload_pfp_to_s3(file, bucket_name, acl="public-read"):
-    try:
-        s3.upload_fileobj(
-            file,
-            bucket_name,
-            f"user_pfp/{file.filename}",
-            ExtraArgs={
-                "ACL": acl,
-                "ContentType": file.content_type
-            }
-        )
-    except Exception as e:
-        print("Something Happened: ", e)
-        return e
-
-    return f"https://{bucket_name}.s3.amazonaws.com/user_pfp/{file.filename}"
 
 @app.errorhandler(403)
 def forbidden(e):
@@ -54,37 +36,30 @@ def register_user():
     email = request.form['email']
     password = request.form['password']
     confirm_password = request.form['confirmPassword']
-    
-    # file = request.files['pfp_url']
-    # if file.filename == '': # No file selected
-    #     pfp_url = 'https://dnd-app-bucket.s3.ap-southeast-2.amazonaws.com/user_pfp/default_pfp.jpg'
-    # else:
-    #     pfp_url = upload_pfp_to_s3(file, bucket_name)
-    
+
     # Check if the email already exists in the DynamoDB table
-    response = table.get_item(Key={'email': email})
+    response = users_table.get_item(Key={'email': email})
     if 'Item' in response:
         error_message = "Sorry! That email is already in use"
         return render_template('register.html', error_message=error_message)
     
     # Check if the username already exists in the DynamoDB table
-    response = table.get_item(Key={'username': username})
+    response = users_table.get_item(Key={'username': username})
     if 'Item' in response:
         error_message = "Sorry! That username is already in use"
         return render_template('register.html', error_message=error_message)
     
     # Check if the passwords match
     if password != confirm_password:
-        error_message = "Sorry! Password and Confirm Password do not match"
+        error_message = "Sorry! Passwords do not match"
         return render_template('register.html', error_message=error_message)
 
-    table.put_item(
+    users_table.put_item(
         Item={
             'username': username,
             'display_name': display_name,
             'email': email,
             'password': password,
-            # 'pfp_url': pfp_url
         }
     )
 
