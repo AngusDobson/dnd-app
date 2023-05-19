38047818bff2998ec2e915073fe83a94bc8351e0
AngusDobson <57667421+AngusDobson@users.noreply.github.com>
2023-05-19 14:00:16 +1000

    Register functionality v2

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect
+import os
 import boto3
-
+from flask import Flask, render_template, request, redirect, url_for
 
 application = Flask(__name__)
 app = application
+app.secret_key = os.urandom(24)
 
 # Configure boto3 client
 boto3.setup_default_session(region_name='ap-southeast-2')
@@ -12,17 +13,6 @@ table = dynamodb.Table('users')
 
 s3 = boto3.client('s3')
 bucket_name = 'dnd-app-bucket'
-
-def create_new_user(username, display_name, email, password, pfp_url):
-    table.put_item(
-       Item={
-            'username': username,
-            'display_name': display_name,
-            'email': email,
-            'password': password,
-            'pfp_url': pfp_url
-        }
-    )
 
 def upload_pfp_to_s3(file, bucket_name, acl="public-read"):
     try:
@@ -41,39 +31,40 @@ def upload_pfp_to_s3(file, bucket_name, acl="public-read"):
 
     return f"https://{bucket_name}.s3.amazonaws.com/user_pfp/{file.filename}"
 
-
 @app.errorhandler(403)
 def forbidden(e):
     return render_template('forbidden.html'), 403
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+    success_message = request.args.get('success_message')
+    return render_template('login.html', success_message=success_message)
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register')
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        display_name = request.form['display_name']
-        email = request.form['email']
-        password = request.form['password']
-        
-        file = request.files['pfp_url']
-        if file.filename == '': # No file selected
-            pfp_url = 'https://dnd-app-bucket.s3.ap-southeast-2.amazonaws.com/user_pfp/default_pfp.jpg'
-        else:
-            pfp_url = upload_pfp_to_s3(file, bucket_name)
-        
-        table.put_item(
-           Item={
-                'username': username,
-                'display_name': display_name,
-                'email': email,
-                'password': password,
-                'pfp_url': pfp_url
-            }
-        )
-
-        return redirect('/')
-    
     return render_template('register.html')
+
+@app.route('/register_user', methods=['GET', 'POST'])
+def register_user():
+    username = request.form['username']
+    display_name = request.form['display_name']
+    email = request.form['email']
+    password = request.form['password']
+        
+    file = request.files['pfp_url']
+    if file.filename == '': # No file selected
+        pfp_url = 'https://dnd-app-bucket.s3.ap-southeast-2.amazonaws.com/user_pfp/default_pfp.jpg'
+    else:
+        pfp_url = upload_pfp_to_s3(file, bucket_name)
+    
+    table.put_item(
+        Item={
+            'username': username,
+            'display_name': display_name,
+            'email': email,
+            'password': password,
+            'pfp_url': pfp_url
+        }
+    )
+
+    return redirect(url_for('home', success_message='Registration successful!'))
